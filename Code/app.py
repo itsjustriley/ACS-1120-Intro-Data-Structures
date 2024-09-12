fb7c93cfe04aa1768db0075775a6cb02a59386a9
itsjustriley <142259664+itsjustriley@users.noreply.github.com>
2024-09-12 17:47:39 -0300

    relocating files, making it a flask app

--- Code/app.py
+++ Code/app.py
@@ -1,17 +1,37 @@
 """Main script, uses other modules to generate sentences."""
 from flask import Flask
+import sample
+import rearrange
+import histogram
+import random
 
 
 app = Flask(__name__)
 
 # TODO: Initialize your histogram, hash table, or markov chain here.
 # Any code placed here will run only once, when the server starts.
+def source_text():
+    with open('dnd5esrd.txt', 'r') as f:
+        return f.read().replace('\n', ' ')
+        
+source = source_text()
 
+new_histogram = histogram.histogram(source)
 
 @app.route("/")
 def home():
-    """Route that returns a web page containing the generated text."""
-    return "<p>TODO: Return a word here!</p>"
+    number_of_words = random.randint(5, 10)
+    words = []
+    for i in range(number_of_words):
+        words.append(sample.weighted_random_word(new_histogram))
+
+    shuffled = rearrange.rearrange_words(words)
+    
+    shuffled = shuffled.capitalize()
+    sentence = shuffled + '.'
+
+    return sentence
+    
 
 
 if __name__ == "__main__":
